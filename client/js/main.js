f7a996c3bf1250ac3083dbe91e3ba5f7a560b085
Wassim Marrakchi <marrakchiwassim@yahoo.fr>
2017-10-21 16:32:37 -0400

    Fetch

--- client/js/main.js
+++ client/js/main.js
@@ -16,7 +16,7 @@ fake_return_json =
    {daysAgo: 6, sentiment: 0.9},
    {daysAgo: 1, sentiment: -0.1},
    {daysAgo: 0, sentiment: -1},
-   {daysAgo: 2, sentiment: -0.4},;
+   {daysAgo: 2, sentiment: -0.4},
    {daysAgo: 3, sentiment: -0.6},
    {daysAgo: 5, sentiment: 0.8},
    {daysAgo: 1, sentiment: 1},
@@ -53,4 +53,30 @@ function getAverageSentimentByDay()
     sentiment[fake_return_json[j].daysAgo].sentiments += fake_return_json[j].sentiment/fake_return_json[j].mentions;
   }
   return sentiment;
+}
+
+function getListCompanies()
+{
+  fetch(url) // Call the fetch function passing the url of the API as a parameter
+  .then(resp) => resp.json())
+  .then(function()
+  {
+    return resp;
+  })
+  .catch(function() {
+    return "We don't have enough data on this company.";
+  });
+}
+
+function getMentionsCompany()
+{
+  fetch(url) // Call the fetch function passing the url of the API as a parameter
+  .then(resp) => resp.json())
+  .then(function()
+  {
+    // Your code for handling the data you get from the API
+  })
+  .catch(function() {
+    // This is where you run code if the server returns any errors
+  });
 }
