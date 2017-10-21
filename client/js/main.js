66ec6b6ff014059da7da492f364a4017cddf5edd
rohanjha <rohan_jha@brown.edu>
2017-10-21 11:15:13 -0400

    changed font and added fake data to work with

---
+++ client/js/main.js
@@ -0,0 +1,34 @@
+fake_return_json =
+  [{daysAgo: 0, sentiment: -0.3},
+   {daysAgo: 2, sentiment: 0},
+   {daysAgo: 3, sentiment: 0.4},
+   {daysAgo: 4, sentiment: 0.8},
+   {daysAgo: 2, sentiment: -0.5},
+   {daysAgo: 1, sentiment: 1},
+   {daysAgo: 3, sentiment: 0.6},
+   {daysAgo: 4, sentiment: 0.2},
+   {daysAgo: 5, sentiment: 0},
+   {daysAgo: 6, sentiment: -0.6},
+   {daysAgo: 7, sentiment: -0.4},
+   {daysAgo: 3, sentiment: 0.2},
+   {daysAgo: 2, sentiment: 0.8},
+   {daysAgo: 5, sentiment: 0.6},
+   {daysAgo: 6, sentiment: 0.9},
+   {daysAgo: 1, sentiment: -0.1},
+   {daysAgo: 0, sentiment: -1},
+   {daysAgo: 2, sentiment: -0.4},;
+   {daysAgo: 3, sentiment: -0.6},
+   {daysAgo: 5, sentiment: 0.8},
+   {daysAgo: 1, sentiment: 1},
+   {daysAgo: 0, sentiment: -0.2},
+   {daysAgo: 2, sentiment: 0.6},
+   {daysAgo: 2, sentiment: 0.7},
+   {daysAgo: 3, sentiment: 0.2}]
+
+function getTotalMentionsByDay() {
+
+}
+
+function getAverageSentimentByDay() {
+  
+}
