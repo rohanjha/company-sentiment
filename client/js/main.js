52bf710205c018ef620bc621d7f0f951cd7ed945
Wassim Marrakchi <marrakchiwassim@yahoo.fr>
2017-10-21 17:15:09 -0400

    Fixed Gets

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,5 @@
+let numberDays = 7;
+
 let fake_return_json =
   [{daysAgo: 0, sentiment: -0.3},
    {daysAgo: 2, sentiment: 0},
@@ -9,7 +11,7 @@ let fake_return_json =
    {daysAgo: 4, sentiment: 0.2},
    {daysAgo: 5, sentiment: 0},
    {daysAgo: 6, sentiment: -0.6},
-   {daysAgo: 7, sentiment: -0.4},
+   {daysAgo: 6, sentiment: -0.4},
    {daysAgo: 3, sentiment: 0.2},
    {daysAgo: 2, sentiment: 0.8},
    {daysAgo: 5, sentiment: 0.6},
@@ -32,11 +34,13 @@ let fake_return_json =
 $(document).ready(initialize);
 $("#search").click(searchClicked);
 
-function initialize() {
+function initialize()
+{
   drawMentionsGraph();
 }
 
-function searchClicked() {
+function searchClicked()
+{
   // TODO: Get rid of the title bar
 }
 
@@ -48,14 +52,13 @@ function searchClicked() {
 
 function getTotalMentionsByDay()
 {
-  mention;
-  for (var j = 0; j < fake_return_json.length; i++)
+  let mention = [];
+  for (var i = 0; i < numberDays; i++)
   {
-    if(!mention[fake_return_json[j].daysAgo])
-    {
-      mention.push({day: 0, mentions:0; sentiments = 0})
-      mention[fake_return_json[j].daysAgo].day = fake_return_json[j].daysAgo;
-    }
+    mention.push({day: i, mentions:0});
+  }
+  for (var j = 0; j < fake_return_json.length; j++)
+  {
     mention[fake_return_json[j].daysAgo].mentions++;
   }
   return mention;
@@ -63,12 +66,12 @@ function getTotalMentionsByDay()
 
 function getAverageSentimentByDay()
 {
-  sentiment;
-  for (var j = 0; j < fake_return_json.length; i++)
+  let sentiment = [];
+  for (var j = 0; j < fake_return_json.length; j++)
   {
     if(!sentiment[fake_return_json[j].daysAgo])
     {
-      sentiment.push({day: 0, sentiments = 0})
+      sentiment.push({day: 0, sentiments: 0})
       sentiment[fake_return_json[j].daysAgo].day = fake_return_json[j].daysAgo;
     }
     sentiment[fake_return_json[j].daysAgo].sentiments += fake_return_json[j].sentiment/fake_return_json[j].mentions;
@@ -78,28 +81,13 @@ function getAverageSentimentByDay()
 
 function getListCompanies()
 {
-  fetch(url) // Call the fetch function passing the url of the API as a parameter
-  .then(resp) => resp.json())
-  .then(function()
-  {
-    return resp;
-  })
-  .catch(function() {
-    return "We don't have enough data on this company.";
-  });
+
 }
 
 function getMentionsCompany()
 {
-  fetch(url) // Call the fetch function passing the url of the API as a parameter
-  .then(resp) => resp.json())
-  .then(function()
-  {
-    // Your code for handling the data you get from the API
-  })
-  .catch(function() {
-    // This is where you run code if the server returns any errors
-  });
+
+}
 
 ///
 // Will be called after the above data is parsed
